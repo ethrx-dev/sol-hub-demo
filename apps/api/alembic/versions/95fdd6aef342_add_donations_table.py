bbd35d161efc0705942285455fa4e45a05950ac3
alchemical1 <alchemical1@blaqbox.meta.local>
2026-07-14 12:57:16 -0700

    Feature 7: Donation Page Activation - Donation model, webhook one-time payment handling, admin donations page + sidebar link

--- apps/api/alembic/versions/95fdd6aef342_add_donations_table.py
+++ apps/api/alembic/versions/95fdd6aef342_add_donations_table.py
@@ -1,8 +1,8 @@
 """add_donations_table
 
-Revision ID: e8a4b67102e1
+Revision ID: 95fdd6aef342
 Revises: a1b2c3d4e5f6
-Create Date: 2026-07-14 12:47:22.319105
+Create Date: 2026-07-14 12:57:03.656353
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'e8a4b67102e1'
+revision: str = '95fdd6aef342'
 down_revision: Union[str, None] = 'a1b2c3d4e5f6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
